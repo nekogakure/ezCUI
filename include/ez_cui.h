e659e9622262be6a04120458ea655eab0accbfaa
ねこがくれ <156896971+nekogakure@users.noreply.github.com>
2025-09-28 15:05:04 +0900

    refactor all file

--- include/ez_cui.h
+++ include/ez_cui.h
@@ -3,10 +3,14 @@
 
 #include "helper.h"
 
-void c_register_command(command_t *cmd);
+void c_register_command(command_t * cmd);
+
 int c_run(int argc, char **argv);
+
 void c_set_version(const char *version);
+
 void c_set_appname(const char *appname);
+
 void c_set_author(const char *author);
 
 #endif // EZ_CUI_H
--- include/helper.h
+++ include/helper.h
@@ -14,12 +14,12 @@ typedef struct args {
  * @brief コマンド情報構造体
  */
 typedef struct {
-    char *name;                   /// コマンド名
-    char *description;            /// コマンド説明
-    struct option *options;       /// オプション配列
-    int option_count;             /// オプション数
-    struct command *subcommands;  /// サブコマンド配列
-    int subcommand_count;         /// サブコマンド数
+    char *name; /// コマンド名
+    char *description; /// コマンド説明
+    struct option *options; /// オプション配列
+    int option_count; /// オプション数
+    struct command *subcommands; /// サブコマンド配列
+    int subcommand_count; /// サブコマンド数
     void (*action)(void *context, struct args *args); /// 実行関数（コマンドが呼び出されたときに実行される関数）
 } command_t;
 
